eb3ae611799c2a69fdddb0c1f0992d9c312b698a
Daniel Tovar <dt@dantovar.com>
2019-03-09 16:15:59 -0700

    finished minimum requirements - still need bonuses

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,28 +10,46 @@ let topics = [
     keyword = undefined;
 
 const renderButtons = () => {
+    $('#buttons-area').empty();
+
     topics.forEach(function(x) {
         $('#buttons-area').append(
-            `<button class="btn btn-lg bg-warning topic">${x}</button>`
+            `<button class="btn btn-lg bg-warning m-1 topic">${x}</button>`
         );
     });
 };
 
 const printImages = x => {
+    $('.area').empty();
     keyword = x.target.innerHTML;
     query = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apikey}&limit=10`;
     $.ajax({ url: query, method: 'GET' }).then(function(res) {
         $.each(res.data, function(_i, val) {
+            console.log(val);
             let still = val.images.original_still.url;
             let gif = val.images.original.url;
             $('.area').append(
-                `<img src="${still}" class="img-container static" data-gif="${gif}" data-still="${still}">`
+                `<img src="${still}" class="img-container static" data-gif="${gif}" data-still="${still}">
+                <p>Rating: ${val.rating.toUpperCase()}</p>`
             );
         });
     });
 };
 
+$('#add-topic').on('keypress', function(event) {
+    if (event.which === 13) {
+        event.preventDefault();
+        let topic = $(this)
+            .val()
+            .trim()
+            .toLowerCase();
+        topics.push(topic);
+        $(this).val('');
+        renderButtons();
+    }
+});
 $(document).on('click', '.topic', printImages);
+
 $(document).on('click', '.img-container', function() {
     let still = $(this).attr('data-still');
     let gif = $(this).attr('data-gif');
