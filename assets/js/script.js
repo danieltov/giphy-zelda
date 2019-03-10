0ff2246ac716ec3f51563e782823b6cf45e54725
Daniel Tovar <dt@dantovar.com>
2019-03-10 13:09:14 -0600

    bonuses 1-5 met

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,8 @@ let topics = [
         'twilight princess'
     ],
     query = undefined,
-    keyword = undefined;
+    keyword = undefined,
+    offset = 0;
 
 const renderButtons = () => {
     $('#buttons-area').empty();
@@ -23,16 +24,42 @@ const printImages = x => {
     $('.area').empty();
     keyword = x.target.innerHTML;
     query = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apikey}&limit=10`;
+    console.log(query);
     $.ajax({ url: query, method: 'GET' }).then(function(res) {
-        $.each(res.data, function(_i, val) {
-            console.log(val);
-            let still = val.images.original_still.url;
-            let gif = val.images.original.url;
+        appendImages(res);
+        if (res.data.length === 10) {
             $('.area').append(
-                `<img src="${still}" class="img-container static" data-gif="${gif}" data-still="${still}">
-                <p>Rating: ${val.rating.toUpperCase()}</p>`
+                `<button class="btn btn-lg bg-danger m-1 text-white gimme">Gimme 10 More!</button>`
             );
-        });
+        } else {
+            $('.area')
+                .append(`<p class="strong text-center alert alert-warning">
+                You've returned all of the gifs!
+            </p>`);
+        }
+    });
+};
+
+const gimmeMore = () => {
+    offset += 10;
+    $.ajax({ url: `${query}&offset=${offset}`, method: 'GET' }).then(function(
+        res
+    ) {
+        appendImages(res);
+        $('.area').append(
+            `<button class="btn btn-lg bg-danger m-1 text-white gimme">Gimme 10 More!</button>`
+        );
+    });
+};
+
+const appendImages = x => {
+    $.each(x.data, function(_i, val) {
+        let still = val.images.original_still.url;
+        let gif = val.images.original.url;
+        $('.area').append(
+            `<img src="${still}" class="img-container static" data-gif="${gif}" data-still="${still}">
+                <p><strong>Rating: ${val.rating.toUpperCase()} <a href="${gif}" target="_blank"><i class="ml-5 fas fa-download"></i></a></strong></p>`
+        );
     });
 };
 
@@ -62,6 +89,11 @@ $(document).on('click', '.img-container', function() {
             .attr('src', still)
             .addClass('static');
     }
+});
+
+$(document).on('click', '.gimme', function() {
+    gimmeMore();
+    $(this).remove();
 });
 
 $(function() {
